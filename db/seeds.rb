e2cef0f0f4d411357e34377faec8366ea26566de
Matthew Stuewe <stuewe.matthew@gmail.com>
2019-05-23 12:29:18 +0900

    Seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Ingredient.destroy_all
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+drink_serialized = open(url).read
+data = JSON.parse(drink_serialized)
+p data['drinks'].first
+data["drinks"].each do |liquor|
+  Ingredient.create!(name: liquor["strIngredient1"])
+end
